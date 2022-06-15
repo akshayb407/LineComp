5ff52492fd6ad593051b7139a611851bab24e52d
akshayb407 <akshayb407@gmail.com>
2022-06-15 17:38:18 +0530

    added master

---
+++ Line/Line/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
