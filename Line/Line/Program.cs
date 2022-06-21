e8d3e70fae12e68a372b2bdcbaffba3227924065
akshayb407 <akshayb407@gmail.com>
2022-06-21 12:25:43 +0530

    UC1 added

--- Line/Line/Program.cs
+++ Line/Line/Program.cs
@@ -12,9 +12,7 @@ namespace Line
             UC1 u1 = new UC1();
             u1.Length();
 
-            Console.WriteLine("===========UC2===========");
-            UC2 u2 = new UC2();
-            u2.EqualityOfLines();
+           
         }
 
 
