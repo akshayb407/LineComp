a1c8329d85197ff1959e9f3c1bcd79a368cc48c4
akshayb407 <akshayb407@gmail.com>
2022-06-21 12:32:53 +0530

    UC2 added

--- Line/Line/Program.cs
+++ Line/Line/Program.cs
@@ -6,13 +6,11 @@ namespace Line
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,Welcome to LineComparision");
 
-            Console.WriteLine("===========UC1===========");
-            UC1 u1 = new UC1();
-            u1.Length();
+            Console.WriteLine("===========UC2===========");
+            UC2 u2 = new UC2();
+            u2.EqualityOfLines();
 
-           
         }
 
 
