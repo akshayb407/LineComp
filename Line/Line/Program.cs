09bf1c48ea373400af96abee0c18142329788bba
akshayb407 <akshayb407@gmail.com>
2022-06-21 13:36:09 +0530

    UC3 added

--- Line/Line/Program.cs
+++ Line/Line/Program.cs
@@ -5,11 +5,10 @@ namespace Line
     internal class Program
     {
         static void Main(string[] args)
-        {
-
-            Console.WriteLine("===========UC2===========");
-            UC2 u2 = new UC2();
-            u2.EqualityOfLines();
+        {            
+      
+            Class1.DisplayMessage();
+            
 
         }
 
