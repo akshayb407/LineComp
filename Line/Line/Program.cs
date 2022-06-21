bccea58b3800d1389d37e40ac6fb2c250d9882ba
akshayb407 <akshayb407@gmail.com>
2022-06-21 13:49:06 +0530

    UC4 added

--- Line/Line/Program.cs
+++ Line/Line/Program.cs
@@ -5,21 +5,10 @@ namespace Line
     internal class Program
     {
         static void Main(string[] args)
+
         {
-
-            Console.WriteLine("Hello,Welcome to LineComparision");
-
-            Console.WriteLine("===========UC1===========");
-            UC1 u1 = new UC1();
-            u1.Length();
-
-
-            Console.WriteLine("===========UC2===========");
-            UC2 u2 = new UC2();
-            u2.EqualityOfLines();
-
-        }
-
+            Class1.DisplayMessage();
+        } 
 
 
     }
